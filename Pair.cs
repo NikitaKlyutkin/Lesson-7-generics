a2219d4ceade2c3dc90e85daadc4a2537517ee60
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-10-26 17:46:09 +0300

    Add project files.

---
+++ Pair.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7_generics
+{
+    internal class Pair<S, T>
+    {
+        public S Name;
+        public T Age;
+        public Pair(S name, T age)
+        {
+           Name = name;
+           Age = age;  
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using Lesson_7_generics;
+
+Pair<string, int> pair = new Pair<string, int>("Nikita", 21);
+Console.WriteLine(pair.Name);
+Console.WriteLine(pair.Age);
